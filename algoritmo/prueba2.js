f0514102ee81c7755b80223fe943ce86fd7ef49c
jua9507 <juandavidnaranjo75@gmail.com>
2021-09-08 22:04:39 -0500

    prueba2 desde algoritmo

---
+++ algoritmo/prueba2.js
@@ -0,0 +1,11 @@
+const animal = {
+    nommbre ='venus',
+    apelledo = 'rivera', 
+    edad = 5,
+    raza:{
+        perro = false,
+        gato = true,
+    }, 
+    aficciones:['corre','dormir','morder']
+        
+    }
